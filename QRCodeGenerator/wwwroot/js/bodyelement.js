22a6e9404160f735ac5c6734a802ec806021e60e
Herman Brunberg <herman.brunberg@utb.ecutbildning.se>
2021-10-22 22:49:15 +0200

    Add project files.

---
+++ QRCodeGenerator/wwwroot/js/bodyelement.js
@@ -0,0 +1,22 @@
+﻿
+var getBodyElement = function() {
+    return document.getElementsByTagName("body")[0];
+}
+
+var addCssClassToBody = function(cssClass) {
+    var body = getBodyElement();
+
+    if (!body.classList.contains(cssClass)) {
+        body.classList.add(cssClass);
+    }
+}
+
+var setLanguageOfBody = function(language) {
+    var body = getBodyElement();
+    body.lang = language;
+}
+
+var setTextDirectionOfBody = function (direction) {
+        var body = getBodyElement();
+        body.dir = direction;
+    }
