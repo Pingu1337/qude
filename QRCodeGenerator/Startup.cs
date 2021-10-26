1979daabf10113753d0c6346e26438f4fbbb8221
Herman Brunberg <herman.brunberg@utb.ecutbildning.se>
2021-10-26 09:59:18 +0200

    fixed overflow issue.
    added some sharing and embed options and fixed with the default colors

--- QRCodeGenerator/Startup.cs
+++ QRCodeGenerator/Startup.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CurrieTechnologies.Razor.Clipboard;
 using MudBlazor.Services;
 
 namespace QRCodeGenerator
@@ -29,6 +30,7 @@ namespace QRCodeGenerator
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddMudServices();
+            services.AddClipboard();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
